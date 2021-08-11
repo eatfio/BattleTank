55e4dd0eed3d505f3ff6217582bdce5bb8a8db9c
ZH.Chen <godczh@gmail.com>
2021-08-11 18:52:52 +0200

    creat some class and do parent
    creat tank class & tankplayercontroller class
    and parent the class to BP

---
+++ BattleTankProject/Source/BattleTankProject/Private/TankPlayerController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "BattleTankProject/BattleTankProject.h"
+#include "TankPlayerController.h"
+
+//ATank* ATankPlayerController::GetControlledTank() const
+//{
+//	return Cast<ATank>(GetPawn());
+//}
